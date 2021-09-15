25f6960a7d5b3f130317e02e3123f1586538b967
xiezhipeng <xiezhipeng.peng@bytedance.com>
2021-09-15 18:26:24 +0800

    配置心跳机制

--- src/main/java/cn/monitor4all/springbootwebsocketdemo/config/WebSocketConfig.java
+++ src/main/java/cn/monitor4all/springbootwebsocketdemo/config/WebSocketConfig.java
@@ -1,17 +1,30 @@
 package cn.monitor4all.springbootwebsocketdemo.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.config.SimpleBrokerRegistration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.*;
 
 
+@Slf4j
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    private static long HEART_BEAT = 3000;
+
     @Autowired
     MyHandShakeHandler myHandShakeHandler;
+
+    /**
+     * 是否开启心跳
+     */
+    @Value("${peng.websocket.stomp.heartbeat}")
+    boolean enableHeaderBeat;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -23,7 +36,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.setApplicationDestinationPrefixes("/app");
 
         // Enables a simple in-memory broker
-        registry.enableSimpleBroker("/topic", "/unique");
+        SimpleBrokerRegistration simpleBrokerRegistration = registry.enableSimpleBroker("/topic", "/unique");
+
+        if (enableHeaderBeat) {
+            enableHeartbeat(simpleBrokerRegistration);
+        }
 
         //   Use this for enabling a Full featured broker like RabbitMQ
         /*
@@ -33,5 +50,19 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 .setClientLogin("guest")
                 .setClientPasscode("guest");
         */
+    }
+
+    /**
+     * 开启心跳机制
+     * @param simpleBrokerRegistration
+     */
+    public void enableHeartbeat(SimpleBrokerRegistration simpleBrokerRegistration) {
+        log.info("开启心跳");
+        //配置心跳任务
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(1);
+        scheduler.setThreadNamePrefix("wss-heartbeat-thread-");
+        scheduler.initialize();
+        simpleBrokerRegistration.setHeartbeatValue(new long[]{HEART_BEAT, HEART_BEAT}).setTaskScheduler(scheduler);
     }
 }
